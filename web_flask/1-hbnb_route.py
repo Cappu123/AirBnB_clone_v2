8b011360a338f39c1a25594c0e3374ea4ee30971
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-01 12:48:48 +0000

    new tab

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,22 @@
-""" Script that starts web application, must lilsten to 0.0.0.0:5000 """
-
+""""Script that starts a Flask web application """
 from flask import Flask
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello_hbnb():
-    """prints the web"""
-    return "Hello HBNB!"
+    """This Prints the  Web """
+    return 'Hello HBNB!'
+
+
 @app.route('/hbnb')
 def hbnb():
-    """prints the hbnb"""
+    """ This Prints the Web """
     return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
