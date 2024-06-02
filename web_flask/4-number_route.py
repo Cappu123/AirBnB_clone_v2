a7655f46f182bc4e11f0fe9593a1424698c409f6
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-02 07:57:59 +0000

    number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,36 +1,40 @@
-""" A script that starts Flask web application """
-
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
 from flask import Flask
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello_hbnb():
-    """ This prints the web """
+    """ Print Web """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    """ This prints the web """
+    """ Print Web """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
-def c_is_fun():   
-    """ Displays char C flollowed by what the variable <text> holds """
+def c_is_fun(text):
+    """ Print a char C followed by the value of the text variable """
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python')
 @app.route('/python/<text>')
-def python_is_cool(text="is cool"):
-    """ Displays "Python" followed by the value of 'text' variable
-    with the default value of text "is cool"
-    """
+def python_is_cool(text='is cool'):
+    """ Print Python, followed by the value of the text variable,
+    with default value of text: is cool """
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 @app.route('/number/<int:n>')
-def is_n_number(n):
-    """ Displays 'n is a number only if n is an integer' """
+def number(n):
+    """ number route """
     return '{:d} is a number'.format(n)
 
 
