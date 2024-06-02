cb250265cc54b676b19a86e6fae168b31d109807
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-02 19:25:06 +0000

    improve

--- models/city.py
+++ models/city.py
@@ -5,23 +5,20 @@
 """
 
 from models.base_model import BaseModel, Base
-from models.state import State
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Integer
 from os import environ
+from sqlalchemy.ext.declarative import declarative_base
 
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+#storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
     """ City class :City class to represent a city
-    City class :City class to represent a city"""
-
-    if (storage_engine == "db"):
-        __tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey(State.id))
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        name = ""
-        state_id = ""
+    City class :City class to represent a city
+    """
+    #from models.state import State
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey(State.id))
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities")
--- models/state.py
+++ models/state.py
@@ -4,29 +4,36 @@
 """
 
 from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
 import models
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 from os import environ
+from models.city import City
+import shlex
 
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+#storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class: class to represent states of cities"""
-    if (storage_engine == 'db'):
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
 
-        @property
-        def cities(self):
-            """cities list
-            """
-            result = []
-            for j, i in models.storage.all(models.city.City).items():
-                if (i.state_id == self.id):
-                    result.append(i)
-            return result
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade='all')
+
+    @property
+    def cities(self):
+        #from models.city import City
+        var = models.storage.all()
+        listl = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                listal.append(var[key])
+        for elem in listl:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
