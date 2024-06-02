a413d446f9542923a1128f3c7ae5e5aeece243f9
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-02 20:30:38 +0000

    all html done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-""" A script that starts a Flask web application """
-
-
+"""script that starts a Flask web application"""
 from flask import Flask
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello_hbnb():
-    """ prints the web """
+    """print web"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    """ prints the web """
+    """print web"""
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """ prints the web """
+    """print C followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
